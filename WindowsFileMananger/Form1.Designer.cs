19ad7de4742cb6e4790ac529434d697b8c7c0ab7
Xander <cleverfairy23@gmail.com>
2021-04-05 16:17:31 +0800

    first commit

---
+++ WindowsFileMananger/Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace WindowsFileMananger
+{
+    partial class FileMananger
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FileMananger));
+            this.listView_Area = new System.Windows.Forms.ListView();
+            this.tbFilePath = new System.Windows.Forms.TextBox();
+            this.btnGoto = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.btnPrevious = new System.Windows.Forms.Button();
+            this.label_perious = new System.Windows.Forms.Label();
+            this.label_next = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listView_Area
+            // 
+            this.listView_Area.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listView_Area.AutoArrange = false;
+            this.listView_Area.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.listView_Area.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.listView_Area.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listView_Area.HideSelection = false;
+            this.listView_Area.Location = new System.Drawing.Point(14, 56);
+            this.listView_Area.Name = "listView_Area";
+            this.listView_Area.Size = new System.Drawing.Size(1095, 652);
+            this.listView_Area.TabIndex = 0;
+            this.listView_Area.UseCompatibleStateImageBehavior = false;
+            this.listView_Area.DoubleClick += new System.EventHandler(this.listView_Area_DoubleClick);
+            this.listView_Area.MouseHover += new System.EventHandler(this.listView_Area_MouseHover);
+            // 
+            // tbFilePath
+            // 
+            this.tbFilePath.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbFilePath.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.tbFilePath.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.tbFilePath.Location = new System.Drawing.Point(6, 6);
+            this.tbFilePath.Name = "tbFilePath";
+            this.tbFilePath.Size = new System.Drawing.Size(917, 22);
+            this.tbFilePath.TabIndex = 1;
+            this.tbFilePath.Text = "d:\\";
+            this.tbFilePath.KeyDown += new System.Windows.Forms.KeyEventHandler(this.tbFilePath_KeyDown);
+            // 
+            // btnGoto
+            // 
+            this.btnGoto.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnGoto.BackgroundImage = global::WindowsFileMananger.Properties.Resources.enter;
+            this.btnGoto.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnGoto.FlatAppearance.BorderSize = 0;
+            this.btnGoto.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnGoto.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGoto.Location = new System.Drawing.Point(1051, 9);
+            this.btnGoto.Name = "btnGoto";
+            this.btnGoto.Size = new System.Drawing.Size(50, 35);
+            this.btnGoto.TabIndex = 4;
+            this.btnGoto.UseVisualStyleBackColor = true;
+            this.btnGoto.Click += new System.EventHandler(this.btnGoto_Click);
+            this.btnGoto.MouseEnter += new System.EventHandler(this.btnGoto_MouseEnter);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.BackColor = System.Drawing.Color.White;
+            this.tableLayoutPanel1.CausesValidation = false;
+            this.tableLayoutPanel1.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Single;
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.tbFilePath, 0, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(108, 8);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.Padding = new System.Windows.Forms.Padding(2);
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(929, 36);
+            this.tableLayoutPanel1.TabIndex = 5;
+            // 
+            // btnNext
+            // 
+            this.btnNext.BackgroundImage = global::WindowsFileMananger.Properties.Resources.next;
+            this.btnNext.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnNext.FlatAppearance.BorderSize = 0;
+            this.btnNext.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnNext.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnNext.ForeColor = System.Drawing.Color.Transparent;
+            this.btnNext.Location = new System.Drawing.Point(60, 12);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(35, 32);
+            this.btnNext.TabIndex = 3;
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            this.btnNext.MouseEnter += new System.EventHandler(this.btnNext_MouseEnter);
+            // 
+            // btnPrevious
+            // 
+            this.btnPrevious.BackColor = System.Drawing.Color.Transparent;
+            this.btnPrevious.BackgroundImage = global::WindowsFileMananger.Properties.Resources.previous;
+            this.btnPrevious.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnPrevious.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnPrevious.FlatAppearance.BorderSize = 0;
+            this.btnPrevious.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPrevious.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnPrevious.ForeColor = System.Drawing.Color.Transparent;
+            this.btnPrevious.Location = new System.Drawing.Point(14, 12);
+            this.btnPrevious.Name = "btnPrevious";
+            this.btnPrevious.Size = new System.Drawing.Size(35, 32);
+            this.btnPrevious.TabIndex = 2;
+            this.btnPrevious.UseVisualStyleBackColor = false;
+            this.btnPrevious.Click += new System.EventHandler(this.btnPrevious_Click);
+            this.btnPrevious.MouseEnter += new System.EventHandler(this.btnPrevious_MouseEnter);
+            // 
+            // label_perious
+            // 
+            this.label_perious.AutoSize = true;
+            this.label_perious.Location = new System.Drawing.Point(134, 46);
+            this.label_perious.Name = "label_perious";
+            this.label_perious.Size = new System.Drawing.Size(0, 12);
+            this.label_perious.TabIndex = 6;
+            this.label_perious.Visible = false;
+            // 
+            // label_next
+            // 
+            this.label_next.AutoSize = true;
+            this.label_next.Location = new System.Drawing.Point(294, 46);
+            this.label_next.Name = "label_next";
+            this.label_next.Size = new System.Drawing.Size(0, 12);
+            this.label_next.TabIndex = 7;
+            this.label_next.Visible = false;
+            // 
+            // FileMananger
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1121, 720);
+            this.Controls.Add(this.label_next);
+            this.Controls.Add(this.label_perious);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.btnGoto);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.btnPrevious);
+            this.Controls.Add(this.listView_Area);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FileMananger";
+            this.Text = "文件管理器";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.FileMananger_KeyDown);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView_Area;
+        private System.Windows.Forms.TextBox tbFilePath;
+        private System.Windows.Forms.Button btnPrevious;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.Button btnGoto;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label_perious;
+        private System.Windows.Forms.Label label_next;
+    }
+}
+
---
+++ WindowsFileMananger/Form1.cs
@@ -0,0 +1,309 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFileMananger
+{
+    public partial class FileMananger : Form
+    {
+        //private List<string> btn_Previous = new List<string>();
+        //private List<string> btn_Next = new List<string>();
+        //private string Btn_Enter { get; set; }
+        private string InitFilePath { get; set; }
+
+        public FileMananger()
+        {
+            InitializeComponent();
+            //this.BackgroundImage = ""
+            InitFileListview();
+            InitFilePath = "C:\\";
+            LoadDir(InitFilePath);
+        }
+
+        // 调用系统cmd开启一个新的进程
+        private void RunCommand(string execName, string command)
+        {
+            using (Process pc = new Process())
+            {
+                pc.StartInfo.FileName = execName;
+                pc.StartInfo.CreateNoWindow = true;
+                pc.StartInfo.RedirectStandardError = true;
+                pc.StartInfo.RedirectStandardInput = true;
+                pc.StartInfo.RedirectStandardOutput = true;
+                pc.StartInfo.UseShellExecute = false;
+                pc.Start();
+                pc.StandardInput.WriteLine(command);
+                pc.StandardInput.WriteLine("exit");
+                pc.StandardInput.AutoFlush = true;
+                Console.WriteLine(pc.StandardOutput.ReadToEnd());
+            }
+        }
+
+        private void DeleteListViewItems()
+        {
+            if (listView_Area.Items.Count > 0)
+            {
+                foreach (ListViewItem item in listView_Area.Items)
+                {
+                    item.Remove();
+                }
+            }
+        }
+
+
+        private void UpdateTextBox(string dirPath)
+        {
+            tbFilePath.Text = dirPath;
+           
+            string f1 = tbFilePath.Text;
+            string[] f2 = f1.Split(':');
+            if (f2.Length == 2 && f2[1] == "") f2[1] = "\\";
+            //Console.WriteLine(f2.Length);
+            //foreach (string i in f2)
+            //{
+            //   Console.Write(i + " ");
+            //}
+            Console.WriteLine();
+
+            Text = "文件管理器 -- " + tbFilePath.Text;
+            label_perious.Text = tbFilePath.Text; // 该控件不可见
+
+            tbFilePath.Text = f2[0].ToUpper() + ":" + f2[1];
+
+            if (tbFilePath.Focus())
+            {
+                tbFilePath.Select(tbFilePath.TextLength, 0);
+                tbFilePath.ScrollToCaret();
+            }
+        }
+
+
+        public void SetWidgetToolTip(Button w, string text)
+        {
+            ToolTip tt = new ToolTip();
+            tt.ShowAlways = true;
+            tt.SetToolTip(w, text);
+        }
+
+        // init window
+        private void InitFileListview()
+        {
+            listView_Area.View = View.Details; // 设置显示模式
+            listView_Area.FullRowSelect = true; // 整行选中
+
+            // 宽度-2表示自动调整宽度
+            listView_Area.Columns.Add("文件名", 300, HorizontalAlignment.Left);
+            listView_Area.Columns.Add("修改日期", 230, HorizontalAlignment.Left);
+            listView_Area.Columns.Add("类型", 150, HorizontalAlignment.Left);
+            listView_Area.Columns.Add("大小", -2, HorizontalAlignment.Left);
+            listView_Area.Columns.Add("位置", 300, HorizontalAlignment.Left); // 隐藏列
+
+            //listView1.Items.Add("java study.pdf");
+            ListViewItem item1 = new ListViewItem("java study.pdf");
+            item1.SubItems.Add("2021-3-7 18:52");
+            item1.SubItems.Add("PDF");
+            item1.SubItems.Add("192kb");
+            //listView1.Items.Add(item1);
+
+            // 创建ImageList, 并添加两个小图标
+            ImageList imagelist = new ImageList();
+            imagelist.ImageSize = new Size(16, 16);
+            imagelist.Images.Add(Properties.myResource.icon_folder);
+            imagelist.Images.Add(Properties.myResource.icon_file);
+
+            listView_Area.SmallImageList = imagelist; // 小图标列表,装载ImageList => ListView
+            //listView1.ColumnClick 
+        }
+
+        // add folder list
+        private void addItem(DirectoryInfo dof, int imageIndex)
+        {
+            ListViewItem item = new ListViewItem(dof.Name, imageIndex);
+            item.SubItems.Add(dof.LastWriteTime.ToString("yyyy/MM/dd HH:mm"));
+            item.SubItems.Add("文件夹");
+            item.SubItems.Add("");
+            item.SubItems.Add(dof.FullName);
+            listView_Area.Items.Add(item);
+        }
+
+        // add file list
+        private void addItem(FileInfo dof, int imageIndex)
+        {
+            ListViewItem item = new ListViewItem(dof.Name, imageIndex);
+            item.SubItems.Add(dof.LastWriteTime.ToString("yyyy/MM/dd HH:mm"));
+            item.SubItems.Add(dof.Extension.ToUpper() + "文件");
+            item.SubItems.Add(dof.Length + " bytes");
+            item.SubItems.Add(dof.DirectoryName);
+            listView_Area.Items.Add(item);
+        }
+
+        // load list UI by folder/file path
+        private DirectoryInfo LoadDir(string dirPath)
+        {
+            dirPath = dirPath.Trim();
+            if (dirPath == "") dirPath = InitFilePath;
+            if (dirPath.Contains('/')) dirPath.Replace('/', '\\');
+            if (Path.GetInvalidFileNameChars().Length != 0) { }
+            //Console.WriteLine(Path.GetInvalidFileNameChars().ToString());
+           
+            try
+            {
+                DirectoryInfo dir = new DirectoryInfo(dirPath);
+                if (dir.Exists)
+                {
+
+                    Console.WriteLine(dir.Attributes.ToString());
+                    UpdateTextBox(dirPath);
+                    DeleteListViewItems();
+                    listView_Area.BeginUpdate();
+                    DirectoryInfo[] subDirs = dir.GetDirectories();
+                    foreach (DirectoryInfo folder in subDirs)
+                    {
+                        string attr = folder.Attributes.ToString();
+                        if (!attr.Contains("System"))  //  && attr.Contains("Hidden") 目录内可能含有Windows隐藏文件,无法访问!!
+                        {
+                            addItem(folder, 0);
+                        }
+                    }
+
+                    DirectoryInfo files = new DirectoryInfo(dir.ToString());
+                    FileInfo[] fileInfos = files.GetFiles();
+                    foreach (FileInfo file in fileInfos)
+                    {
+                        addItem(file, 1);
+                    }
+                    listView_Area.EndUpdate();
+                    return dir;
+                }
+                else
+                {
+                    //MessageBox.Show("文件夹或文件路径不存在!!", "!路径不存在");
+
+                    tbFilePath.Text = label_perious.Text;
+                    if (tbFilePath.Focus())
+                    {
+                        tbFilePath.Select(tbFilePath.TextLength, 0);
+                        tbFilePath.ScrollToCaret();
+                    }
+                    return null;
+                }
+            }
+            catch (Exception e)
+            {
+                string err = string.Format("系统错误信息:{0}\n无法访问:{1}\n\n\n", e.Message, tbFilePath.Text);
+                tbFilePath.Text = InitFilePath;
+                LoadDir(tbFilePath.Text);
+                MessageBox.Show(err, "!位置不可用");
+                return null;
+            }
+        }
+
+        // 后退
+        private void btnPrevious_Click(object sender, EventArgs e)
+        {
+            label_next.Text = label_perious.Text;
+            if (label_perious.Text.EndsWith(":\\")) return;
+            string p = new DirectoryInfo(label_perious.Text).Parent.FullName.ToString();
+            LoadDir(p);
+        }
+
+        // 前进
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            if (label_next.Text == "") return;
+            LoadDir(label_next.Text);
+            //Process.Start("cmd"); // 直接打开程序
+            //RunCommand("cmd", "ping -c1 google.com");
+        }
+
+        // 进入文件目录
+        private void btnGoto_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("load file path: " + tbFilePath.Text);
+            if (tbFilePath.Text.EndsWith(":")) tbFilePath.Text = tbFilePath.Text + "\\";
+            LoadDir(tbFilePath.Text);
+        }
+
+        private void tbFilePath_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode.ToString() == "Return")
+            {
+                Console.WriteLine("e.KeyValue: " + e.KeyValue);
+                btnGoto_Click(sender, e);
+            }
+        }
+
+
+        // Hover tip
+        private void btnPrevious_MouseEnter(object sender, EventArgs e)
+        {
+            this.SetWidgetToolTip(btnPrevious, "后退 (Alt + 向左键)");
+        }
+
+        private void btnNext_MouseEnter(object sender, EventArgs e)
+        {
+            this.SetWidgetToolTip(btnNext, "后退 (Alt + 向右键)");
+        }
+
+        private void btnGoto_MouseEnter(object sender, EventArgs e)
+        {
+            this.SetWidgetToolTip(btnGoto, "进入 (Enter)");
+        }
+
+        private void FileMananger_KeyDown(object sender, KeyEventArgs e)
+        {
+            tbFilePath_KeyDown(sender, e);
+            Console.WriteLine("btnGoto_KeyDown <Enter>");
+        }
+
+        private void listView_Area_ColumnClick(object sender, ColumnClickEventArgs e)
+        {
+            Console.WriteLine("选中了 {0} 列", e.Column);
+        }
+
+        private void listView_Area_DoubleClick(object sender, EventArgs e)
+        {
+            if (listView_Area.FocusedItem != null)
+            {
+                if (listView_Area.SelectedItems.Count > 0)
+                {
+                    // get current selected row path
+                    string current_row_5col_text = listView_Area.FocusedItem.SubItems[4].Text;
+                    string currentFilePath = current_row_5col_text + "\\" + listView_Area.FocusedItem.SubItems[0].Text;
+                    if (File.Exists(currentFilePath)) // 文件双击直接打开
+                    {
+                        if (currentFilePath.Contains(" ")) currentFilePath = $"'{currentFilePath}'";
+                        Console.WriteLine("file: " + currentFilePath);
+                        RunCommand("cmd", currentFilePath);
+                    } 
+                    else if (Directory.Exists(current_row_5col_text)) // 目录双击直接进入内层
+                    {
+                        Console.WriteLine("directory: " + current_row_5col_text);
+                        LoadDir(current_row_5col_text);
+                        label_next.Text = "";
+                    }
+                }
+            }
+        }
+
+        // 悬停提示
+        private void listView_Area_MouseHover(object sender, EventArgs e)
+        {
+            foreach (ListViewItem item in listView_Area.Items)
+            {
+                if (item.Focused)
+                {
+                    item.ToolTipText = "item.Text";
+                }
+            }
+        }
+    }
+}
